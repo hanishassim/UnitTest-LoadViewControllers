a74b09f0d8dc99ecd9507d323930396d1cbb84e0
Hanis <hanishassim@users.noreply.github.com>
2024-08-26 10:02:37 +0800

    Created new viewcontroller Add test zero

---
+++ LoadViewControllers/StoryboardBasedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StoryboardBasedViewController.swift
+//  LoadViewControllers
+//
+//  Created by Hassim, Muhammad Hanis on 26/08/2024.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ LoadViewControllersTests/StoryboardBasedViewControllerTests.swift
@@ -0,0 +1,8 @@
+@testable import LoadViewControllers
+import XCTest
+
+final class StoryboardBasedViewControllerTests: XCTestCase {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in StoryboardBasedViewControllerTests")
+    }
+}
